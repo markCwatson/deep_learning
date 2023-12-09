19c110dbe762fd835ddab5c0aa60432b2058b8bc
Mark Watson <markus.c.watson@gmail.com>
2023-12-09 07:31:27 -0400

    small cleanup of imports

--- coursera/deep_learning_specialization/c2-improving_deep_neural_networks/w1a3-gradient_checking/main.py
+++ coursera/deep_learning_specialization/c2-improving_deep_neural_networks/w1a3-gradient_checking/main.py
@@ -4,8 +4,6 @@
 # # Gradient Checking
 
 import numpy as np
-from testCases import *
-from public_tests import *
 from gc_utils import sigmoid, relu, dictionary_to_vector, vector_to_dictionary, gradients_to_vector
 
 """
